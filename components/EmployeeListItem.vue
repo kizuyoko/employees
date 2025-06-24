cf6f58ef5564b060a5c31748121567ef3297685e
kizuyoko <kizuyoko@gmail.com>
2025-06-24 17:43:20 +0200

    Disabled button style and some more css

--- components/EmployeeListItem.vue
+++ components/EmployeeListItem.vue
@@ -6,8 +6,8 @@
       class="object-cover w-16 h-16 rounded-full"
     />
     <div>
-      <h2 class="text-lg font-semibold">{{ name }}</h2>
-      <a :href="`mailto:${email}`" class="text-blue-500 hover:underline" :aria-label="`Skicka e-post till ${name}`">
+      <h2>{{ name }}</h2>
+      <a :href="`mailto:${email}`" :aria-label="`Skicka e-post till ${name}`">
         e-postadress
       </a>
     </div>
